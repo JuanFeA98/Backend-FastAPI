c1791e18b15102137c6a215940a33419b1eafeff
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-09-16 23:37:29 -0500

    Testing pandas

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import pandas as pd
+
+df = pd.read_csv('https://gist.githubusercontent.com/JuanFeA98/ce470cf42a2b4449a314adef81211bf5/raw/d014b5d9cd080d99af363c2e511e16a84fc92cf3/CSS_colors.csv')
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello FastAPI"}
+    return {'mesagge':'Hello'}
+
+@app.get("/colors")
+async def root():
+    return df
