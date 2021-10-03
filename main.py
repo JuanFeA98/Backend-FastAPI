4dcfac0ce93b14001a21a2c785dbd47f3a39fb6a
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-02 23:43:25 -0500

    Organizando el proyecto

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 import pandas as pd
 
-df = pd.read_csv('https://gist.githubusercontent.com/JuanFeA98/ce470cf42a2b4449a314adef81211bf5/raw/d014b5d9cd080d99af363c2e511e16a84fc92cf3/CSS_colors.csv')
-
 app = FastAPI()
 
+# Operación GET sencilla
 @app.get("/")
 async def root():
     return {'mesagge':'Hello'}
 
+# Operación GET desde un archivo remoto
 @app.get("/colors")
-async def root():
+async def colors():
+    df = pd.read_csv('https://gist.githubusercontent.com/JuanFeA98/ce470cf42a2b4449a314adef81211bf5/raw/d014b5d9cd080d99af363c2e511e16a84fc92cf3/CSS_colors.csv')
     return df
+
+# Operación GET con parametros (Existe la opción de establecer el formato de los parametros)
+@app.get("/items_num/{item_id}")
+async def root(item_id: int):
+    return {f'item id {item_id}': (item_id)}
